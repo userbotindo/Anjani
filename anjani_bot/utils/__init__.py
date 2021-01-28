a25b78085ab08858df2632852fa14b0ca6c89024
Mr.Miss <keselekpermen@mrmiss.wtf>
2021-01-28 20:53:22 +0700

    add extractor tools

--- anjani_bot/utils/__init__.py
+++ anjani_bot/utils/__init__.py
@@ -14,5 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .admin_check import adminlist
+from .admin_check import adminlist, user_ban_protected
+from .extractor import extract_user_and_text, extract_user
 from .tools import format_integer, get_readable_time, nekobin
