3e06944dc4fc0fe2046bed7265730a3a2875adcf
Adek Maulana <adek@techdro.id>
2021-07-05 16:42:44 +0700

    Anjani: make color log optional by default
    Change-Id: I12a123adf1463197eb2d19593fc6a344a9c97417

--- anjani/log.py
+++ anjani/log.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import colorlog
 
@@ -7,12 +8,18 @@ level = logging.INFO
 
 def setup_log() -> None:
     """Configures logging"""
-    # Check if running on container
     logging.root.setLevel(level)
 
-    formatter = colorlog.ColoredFormatter(
-        "  %(log_color)s%(levelname)-7s%(reset)s  |  "
-        "%(name)-11s  |  %(log_color)s%(message)s%(reset)s")
+    # Color log config
+    log_color: bool = os.environ.get("LOG_COLOR") in {"enable", 1, "1", "true"}
+
+    if log_color:
+        formatter = colorlog.ColoredFormatter(
+            "  %(log_color)s%(levelname)-7s%(reset)s  |  "
+            "%(name)-11s  |  %(log_color)s%(message)s%(reset)s")
+    else:
+        formatter = logging.Formatter(
+            "  %(levelname)-7s  |  %(name)-11s  |  %(message)s")
     stream = logging.StreamHandler()
     stream.setLevel(level)
     stream.setFormatter(formatter)
