9b1e32cb12ab1f470ab7d9e69dac8a61de7ee7ca
Mr.Miss <hi@mrmiss.me>
2021-07-18 20:10:20 +0700

    plugin: text: apply recursion for cleaner code
    + raise an exception when failed to format the string

--- anjani/plugin.py
+++ anjani/plugin.py
@@ -2,6 +2,7 @@ import codecs
 import inspect
 import logging
 import os.path
+from asyncio.tasks import run_coroutine_threadsafe
 from functools import wraps
 from typing import TYPE_CHECKING, Any, ClassVar, Optional, Protocol
 
@@ -12,23 +13,19 @@ if TYPE_CHECKING:
 
 
 class Func(Protocol):
-    def __call__(_self,
-                 self: "Plugin",
-                 chat_id: int,
-                 text_name: str,
-                 *args: Any,
-                 **kwargs: Any) -> str: ...
+    def __call__(
+        _self, self: "Plugin", chat_id: int, text_name: str, *args: Any, **kwargs: Any
+    ) -> str:
+        ...
 
 
 def loop_safe(func: Func):  # Let default typing choose the return type
     """ Decorator for text methods """
 
     @wraps(func)
-    async def wrapper(self: "Plugin",
-                      chat_id: int,
-                      text_name: str,
-                      *args: Any,
-                      **kwargs: Any) -> str:
+    async def wrapper(
+        self: "Plugin", chat_id: int, text_name: str, *args: Any, **kwargs: Any
+    ) -> str:
         return await util.run_sync(func, self, chat_id, text_name, *args, **kwargs)
 
     return wrapper
@@ -51,35 +48,66 @@ class Plugin:
         self.comment = None
 
     @loop_safe
-    def text(self, chat_id: int, text_name: str, *args: Any, **kwargs: Any) -> str:
-        data = self.bot.chats_languages.get(chat_id, "en")
-        noformat = bool(kwargs.get("noformat", False))
+    def text(
+        self,
+        chat_id: int,
+        text_name: str,
+        *args: Any,
+        noformat: bool = False,
+        _recurse: bool = False,
+        **kwargs: Any,
+    ) -> str:
+        """Parse the string with user language setting.
+        Parameters:
+            chat_id (`int`):
+                Id of the sender(PM's) or chat_id to fetch the user language setting.
+            text_name (`str`):
+                String name to parse. The string is parsed from YAML documents.
+            *args (`any`, *Optional*):
+                One or more values that should be formatted and inserted in the string.
+                The value should be in order based on the language string placeholder.
+            noformat (`bool`, *Optional*):
+                If exist and True, the text returned will not be formated.
+                Default to False.
+            **kwargs (`any`, *Optional*):
+                One or more keyword values that should be formatted and inserted in the string.
+                based on the keyword on the language strings.
+        """
+        if _recurse:
+            data = "en"
+        else:
+            data = self.bot.chats_languages.get(chat_id, "en")
 
         try:
-            text = codecs.decode(
-                codecs.encode(
-                    self.bot.languages[data][text_name], "latin-1", "backslashreplace"
-                ),
-                "unicode-escape",
-            )
-            return text if noformat else text.format(*args, **kwargs)
-        except KeyError:
-            self.bot.log.warning(f"NO LANGUAGE STRING FOR {text_name} in {data}")
+            if data in self.bot.languages and text_name in self.bot.languages[data]:
+                text = codecs.decode(
+                    codecs.encode(
+                        self.bot.languages[data][text_name], "latin-1", "backslashreplace"
+                    ),
+                    "unicode-escape",
+                )
+                return text if noformat else text.format(*args, **kwargs)
+            if _recurse:
+                return (
+                    f"**NO LANGUAGE STRING FOR {text_name} in {data}**\n"
+                    "__Please forward this to @userbotindo__"
+                )
+            text = f"NO LANGUAGE STRING FOR {text_name} in {data}"
+            self.bot.log.warning(text)
+            if data == "en":
+                return text
+        except (IndexError, KeyError):
+            text = f"Failed to format {text_name} string on {data}\n"
+            self.bot.log.error(text)
+            raise
 
-        # Try to send language text in en
-        try:
-            text = codecs.decode(
-                codecs.encode(
-                    self.bot.languages["en"][text_name], "latin-1", "backslashreplace"
-                ),
-                "unicode-escape",
-            )
-            return text if noformat else text.format(*args, **kwargs)
-        except KeyError:
-            return (
-                f"**NO LANGUAGE STRING FOR {text_name} in {data}**\n"
-                "__Please forward this to @userbotindo__"
-            )
+        # If we're here it means that there is no string data for the text_name
+        # Try to send fallback string in english
+        if data != "en":
+            return run_coroutine_threadsafe(
+                self.text(chat_id, text_name, *args, noformat, _recurse=True, **kwargs),
+                self.bot.loop,
+            ).result()
 
     @classmethod
     def format_desc(cls, comment: Optional[str] = None):
