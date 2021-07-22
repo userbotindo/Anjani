94a0b2cbaa1cac5128dd52f69e10b2ee032f79f1
Adek Maulana <adek@techdro.id>
2021-07-23 03:27:26 +0700

    Anjani: Action: fix typing more
    Change-Id: I923c1b2fb28e7f79254541b8636f20a0e44e185a

--- anjani/action.py
+++ anjani/action.py
@@ -1,5 +1,6 @@
 import asyncio
-from typing import TYPE_CHECKING
+from types import TracebackType
+from typing import TYPE_CHECKING, Optional, Type
 
 if TYPE_CHECKING:
     from anjani.command import Context
@@ -10,13 +11,13 @@ class BotAction:
     action: str
     loop: asyncio.AbstractEventLoop
 
-    def __init__(self, ctx: "Context", action: str = "typing"):
+    def __init__(self, ctx: "Context", action: str = "typing") -> None:
         self.context = ctx
         self.action = action
 
         self.loop = ctx.bot.loop
 
-    async def do_action(self):
+    async def do_action(self) -> None:
         chat = self.context.chat
         send = self.context.bot.client.send_chat_action
 
@@ -24,22 +25,32 @@ class BotAction:
             await send(chat.id, self.action)
             await asyncio.sleep(1)
 
-    async def cancel(self):
+    async def cancel(self) -> None:
         chat = self.context.chat
         send = self.context.bot.client.send_chat_action
         await send(chat.id, "cancel")
 
-    def __enter__(self):
+    def __enter__(self) -> "BotAction":
         self.task = self.loop.create_task(self.do_action())
         return self
 
-    def __exit__(self, exc_type, exc, tb):
+    def __exit__(
+        self,
+        exc_type: Optional[Type[Exception]],
+        exc: Optional[Exception],
+        tb: Optional[TracebackType]
+    ) -> None:
         self.task.cancel()
         self.loop.create_task(self.cancel())
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> "BotAction":
         return self.__enter__()
 
-    async def __aexit__(self, exc_type, exc, tb):
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[Exception]],
+        exc: Optional[Exception],
+        tb: Optional[TracebackType]
+    ) -> None:
         self.task.cancel()
         await self.cancel()
