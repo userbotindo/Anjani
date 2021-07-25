db60210f2ef0810d614ea054bde68d468bb2919d
Adek Maulana <adek@techdro.id>
2021-07-26 02:45:11 +0700

    Anjani: Plugins: add miscs
    Change-Id: I747b1e648375fab6fb109155c7183b05d75ee8e4

---
+++ anjani/plugins/misc.py
@@ -0,0 +1,77 @@
+"""miscellaneous bot commands"""
+# Copyright (C) 2020 - 2021  UserbotIndo Team, <https://github.com/userbotindo.git>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import ClassVar, Optional
+
+from pyrogram import filters
+
+from anjani import command, plugin
+
+
+class Misc(plugin.Plugin):
+    name: ClassVar[str] = "Miscs"
+    helpable: ClassVar[bool] = True
+
+
+    async def cmd_id(self, ctx: command.Context) -> str:
+        """Display ID's"""
+        msg = ctx.msg.reply_to_message or ctx.msg
+        out_str = f"👥 **Chat ID :** `{(msg.forward_from_chat or msg.chat).id}`\n"
+        out_str += f"💬 **Message ID :** `{msg.forward_from_message_id or msg.message_id}`\n"
+        if msg.from_user:
+            out_str += f"🙋‍♂️ **From User ID :** `{msg.from_user.id}`\n"
+        file = (
+            msg.audio
+            or msg.animation
+            or msg.document
+            or msg.photo
+            or msg.sticker
+            or msg.voice
+            or msg.video_note
+            or msg.video
+        ) or None
+        if file:
+            out_str += f"📄 **Media Type :** `{file.__class__.__name__}`\n"
+            out_str += f"📄 **File ID :** `{file.file_id}`"
+
+        return out_str
+
+    @command.filters(filters.private)
+    async def cmd_source(self, ctx: command.Context) -> None:
+        """Send the bot source code"""
+        await ctx.respond(
+            "[GitHub repo](https://github.com/userbotindo/Anjani)\n"
+            + "[Support](https://t.me/userbotindo)",
+            disable_web_page_preview=True,
+        )
+
+    @command.filters(filters.group)
+    async def cmd_slap(self, ctx: command.Context) -> Optional[str]:
+        """Slap member with neko slap."""
+        text = ctx.input
+        chat = ctx.msg.chat
+        async with self.bot.http.get("https://www.nekos.life/api/v2/img/slap") as slap:
+            if slap.status != 200:
+                return await self.text(chat.id, "err-api-down")
+            res = await slap.json()
+
+        msg = ctx.msg.reply_to_message or ctx.msg
+        await self.bot.client.send_animation(
+            chat.id,
+            res["url"],
+            reply_to_message_id=msg.message_id,
+            caption=text,
+        )
