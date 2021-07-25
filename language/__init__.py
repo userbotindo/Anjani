720011e5093c80766d64e272ad1239d9d3bbec59
Adek Maulana <adek@techdro.id>
2021-07-26 02:29:03 +0700

    Language: use suffix instead of name.endswith
    Change-Id: I48a4bdb3261f3315a9af05b99e4e5a027901f2df

--- language/__init__.py
+++ language/__init__.py
@@ -5,5 +5,5 @@ from aiopath import AsyncPath
 
 async def getLangFile() -> AsyncIterator[AsyncPath]:
     async for language_file in AsyncPath("language").iterdir():
-        if language_file.name.endswith(".yml"):
+        if language_file.suffix == ".yml":
             yield language_file
