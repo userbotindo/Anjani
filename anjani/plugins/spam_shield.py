456fa94032778d71384cad4a26aee1db05616dae
Adek Maulana <adek@techdro.id>
2021-07-29 18:36:04 +0700

    Anjani: Plugins: SpamShield: cleanup and fixing
    Change-Id: I4a2ffb60d86f59b0b2b25b12eb6f7959edf83291

--- anjani/plugins/spam_shield.py
+++ anjani/plugins/spam_shield.py
@@ -15,39 +15,35 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import asyncio
+from datetime import datetime
 from typing import Any, ClassVar, MutableMapping, Optional
 
-import spamwatch
+from aiohttp import ClientResponseError
 from motor.motor_asyncio import AsyncIOMotorCollection
-from pyrogram import StopPropagation, filters
+from pyrogram import filters
 from pyrogram.errors import ChannelPrivate
 from pyrogram.types import Message, User
-from spamwatch.types import Ban
 
-from anjani import command, listener, plugin, util
+from anjani import command, listener, plugin
 from anjani.custom_filter import admin_only
 
 
 class SpamShield(plugin.Plugin):
     name: ClassVar[str] = "SpamShield"
 
-    client: spamwatch.Client
     db: AsyncIOMotorCollection
-    key: str
+    token: str
 
     async def on_load(self) -> None:
         try:
-            self.key = self.bot.config["sw_api"]
+            self.token = self.bot.config["sw_api"]
         except AttributeError:
-            self.bot.log.warning("SpamWatch API key not exist")
+            self.bot.log.warning("SpamWatch API token not exist")
             return self.bot.unload_plugin(self)
 
         self.db = self.bot.db.get_collection("GBAN_SETTINGS")
-        self.client = spamwatch.Client(self.key)
-        self.count = 1
 
     async def on_chat_migrate(self, message: Message) -> None:
-        self.log.info("migrate")
         new_chat = message.chat.id
         old_chat = message.migrate_from_chat_id
 
@@ -55,7 +51,6 @@ class SpamShield(plugin.Plugin):
             {"chat_id": old_chat},
             {"$set": {"chat_id": new_chat}},
         )
-        self.count += 1
 
     async def on_plugin_backup(self, chat_id: int) -> MutableMapping[str, Any]:
         return {self.name: await self.db.find_one({"chat_id": chat_id},
@@ -67,7 +62,7 @@ class SpamShield(plugin.Plugin):
                                  upsert=True)
 
     async def on_chat_action(self, message: Message) -> None:
-        self.log.info('action')
+        """Checker service for new member"""
         if message.left_chat_member:
             return
         chat = message.chat
@@ -87,7 +82,7 @@ class SpamShield(plugin.Plugin):
 
     @listener.filters(filters.group)
     async def on_message(self, message: Message) -> None:
-        """Checker service"""
+        """Checker service for message"""
         chat = message.chat
         if not chat or message.left_chat_member:
             return
@@ -103,13 +98,47 @@ class SpamShield(plugin.Plugin):
                     return
 
                 target = await chat.get_member(user.id)
-                if (target.status not in {"creator", "administrator"} or
-                        target.user.id not in self.bot.staff):
-                    return await self.check(user, chat.id)
+                if (target.status in {"creator", "administrator"} or
+                        target.user.id in self.bot.staff):
+                    return
+
+                return await self.check(target.user, chat.id)
             else:
                 return
         except ChannelPrivate:
             return
+
+    async def get_ban(self, user_id: int) -> MutableMapping[str, Any]:
+        path = f"https://api.spamwat.ch/banlist/{user_id}"
+        headers = {"Authorization": f"Bearer {self.token}"}
+        async with self.bot.http.get(path, headers=headers) as resp:
+            if resp.status == 200 or resp.status == 201:
+                return await resp.json()
+            if resp.status == 204:
+                return {}
+            if resp.status == 401:
+                raise ClientResponseError(
+                    resp.request_info,
+                    resp.history,
+                    message="Make sure your Spamwatch API token is corret"
+                )
+            elif resp.status == 403:
+                raise ClientResponseError(
+                    resp.request_info,
+                    resp.history,
+                    message="Forbidden, your token permissions is not valid"
+                )
+            elif resp.status == 404:
+                return {}
+            elif resp.status == 429:
+                until = (await resp.json()).get("until", 0)
+                raise ClientResponseError(
+                    resp.request_info,
+                    resp.history,
+                    message=f"Too many requests. Try again in {until - datetime.now()}"
+                )
+            else:
+                raise ClientResponseError(resp.request_info, resp.history)
 
     async def cas_check(self, user_id: int) -> Optional[str]:
         """Check on CAS"""
@@ -118,7 +147,7 @@ class SpamShield(plugin.Plugin):
             return "https://cas.chat/query?u={}".format(user_id) if data["ok"] else None
 
     async def is_active(self, chat_id: int) -> bool:
-        """Return Spam_Shield setting"""
+        """Return SpamShield setting"""
         data: Optional[
             MutableMapping[
                 str,
@@ -142,7 +171,7 @@ class SpamShield(plugin.Plugin):
     async def check(self, user: User, chat_id: int) -> None:
         """Shield checker action."""
         cas, sw = await asyncio.gather(self.cas_check(user.id),
-                                       util.run_sync(self.client.get_ban, user.id))
+                                       self.get_ban(user.id))
         if not cas or not sw:
             return
 
@@ -155,14 +184,10 @@ class SpamShield(plugin.Plugin):
         if sw:
             if not banner:
                 banner = "[Spam Watch](t.me/SpamWatch)"
-                if isinstance(sw, Ban):
-                    reason = sw.reason
-                else:
-                    reason = "__Not specified__"
+                reason = sw["reason"]
             else:
                 banner += " & [Spam Watch](t.me/SpamWatch)"
-                if isinstance(sw, Ban):
-                    reason += f"& {sw.reason}"
+                reason += " & " + sw["reason"]
 
         text = await self.text(chat_id, "banned-text", userlink, user.id, reason, banner)
         await asyncio.gather(
@@ -174,7 +199,6 @@ class SpamShield(plugin.Plugin):
                 disable_web_page_preview=True,
             )
         )
-        raise StopPropagation
 
     @command.filters(admin_only)
     async def cmd_spamshield(self, ctx: command.Context) -> str:
@@ -184,13 +208,10 @@ class SpamShield(plugin.Plugin):
             return await self.text(chat.id, "spamshield-view", await self.is_active(chat.id))
 
         cmd = ctx.args[0].lower()
-        enable: bool
         if cmd in {"on", "true", "enable"}:
             enable = True
-            await self.setting(chat.id, True)
         elif cmd in {"off", "false", "disable"}:
             enable = False
-            await self.setting(chat.id, False)
         else:
             return await self.text(chat.id, "err-invalid-option")
 
