6647e3339d8109923e5228b7a9f78c908e64a206
Adek Maulana <adek@techdro.id>
2021-07-30 20:09:13 +0700

    Anjani: Converter: use the second arg if the first arg is None
    Change-Id: If027f5921b7afe5980e93e7d6ffccaa38ad7b443

--- anjani/util/converter.py
+++ anjani/util/converter.py
@@ -198,9 +198,13 @@ async def parse_arguments(sig: inspect.Signature, ctx: Context) -> List[Any]:
 
         # Check if the annotation was an `Optional` or `Union` type.
         # This type hinting make a parsing ambiguities.
-        # Hence we just simply use the first arg as the converter.
+        # Hence we just simply use the first arg as the converter if is not None type.
+        # Else use the second arg.
         if getattr(converter, "__origin__", None) is Union:
-            converter = converter.__args__[0]
+            if converter.__args__[0] is None:
+                converter = converter.__args__[1]
+            else:
+                converter = converter.__args__[0]
 
         try:
             module = converter.__module__
