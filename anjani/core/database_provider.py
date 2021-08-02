5468f2cfd2641fcc7db5ffbd1077dfaf585ac041
Adek Maulana <adek@techdro.id>
2021-08-02 21:23:47 +0700

    Anjani: DB: add driver info
    Change-Id: Id865766d986761feece58d495e36cea82b71353d

--- anjani/core/database_provider.py
+++ anjani/core/database_provider.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING, Any
 from .anjani_mixin_base import MixinBase
 from anjani import util
 
+from pymongo.driver_info import DriverInfo
+
 if TYPE_CHECKING:
     from .anjani_bot import Anjani
 
@@ -11,8 +13,11 @@ class DatabaseProvider(MixinBase):
     db: util.db.AsyncDB
 
     def __init__(self: "Anjani", **kwargs: Any):
-        client = util.db.AsyncClient(self.config["db_uri"], connect=False)
-        self.db = client.get_database("AnjaniBot")
+        driver = DriverInfo("AsyncIOMongoDB", "alpha", "AsyncIO")
+        client = util.db.AsyncClient(self.config["db_uri"],
+                                     connect=False,
+                                     driver=driver)
+        self.db = client["AnjaniBot"]
 
         # Propagate initialization to other mixins
         super().__init__(**kwargs)
