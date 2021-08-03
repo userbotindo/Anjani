cb94ea049442d733e3130dc1a78ab91e41343251
Adek Maulana <adek@techdro.id>
2021-08-03 20:19:57 +0700

    Anjani: Database: remove DriverInfo from core
    Change-Id: I0f2d55052e704c56d8266926b87ea15b7474c0c3

--- anjani/core/database_provider.py
+++ anjani/core/database_provider.py
@@ -3,8 +3,6 @@ from typing import TYPE_CHECKING, Any
 from .anjani_mixin_base import MixinBase
 from anjani import util
 
-from pymongo.driver_info import DriverInfo
-
 if TYPE_CHECKING:
     from .anjani_bot import Anjani
 
@@ -13,10 +11,7 @@ class DatabaseProvider(MixinBase):
     db: util.db.AsyncDB
 
     def __init__(self: "Anjani", **kwargs: Any):
-        driver = DriverInfo("AsyncIOMongoDB", "alpha", "AsyncIO")
-        client = util.db.AsyncClient(self.config["db_uri"],
-                                     connect=False,
-                                     driver=driver)
+        client = util.db.AsyncClient(self.config["db_uri"], connect=False)
         self.db = client["AnjaniBot"]
 
         # Propagate initialization to other mixins
