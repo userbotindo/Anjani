e0a44bd399046aff5b34e3c7a24a63585952a2e9
Adek Maulana <adek@techdro.id>
2021-09-03 10:11:54 +0700

    Anjani: fix miss merged
    Change-Id: I39e311c05874676de3facc0cf79ce05ec3366131

--- anjani/plugins/rules.py
+++ anjani/plugins/rules.py
@@ -14,70 +14,80 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import asyncio
+from typing import Any, MutableMapping, Optional
 
-from anjani_bot import listener, plugin
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+from anjani import command, filters, plugin, util
 
 
 class Rules(plugin.Plugin):
     name = "Rules"
     helpable = True
 
-    async def __on_load__(self):
-        self.rules_db = self.bot.get_collection("RULES")
+    db: util.db.AsyncCollection
 
-    async def __migrate__(self, old_chat, new_chat):
-        await self.rules_db.update_one(
+    async def on_load(self) -> None:
+        self.db = self.bot.db.get_collection("RULES")
+
+    async def on_chat_migrate(self, message: Message) -> None:
+        new_chat = message.chat.id
+        old_chat = message.migrate_from_chat_id
+
+        await self.db.update_one(
             {"chat_id": old_chat},
             {"$set": {"chat_id": new_chat}},
         )
 
-    async def __backup__(self, chat_id, data=None):
-        if data and data.get(self.name):
-            await self.rules_db.update_one(
-                {"chat_id": chat_id}, {"$set": data[self.name]}, upsert=True
-            )
-        elif not data:
-            return await self.rules_db.find_one({"chat_id": chat_id}, {"_id": False})
+    async def on_plugin_backup(self, chat_id: int) -> MutableMapping[str, Any]:
+        rules = await self.db.find_one({"chat_id": chat_id}, {"_id": False})
+        if not rules:
+            return {}
 
-    @listener.on("setrules", admin_only=True)
-    async def set_rules(self, message):
-        chat_id = message.chat.id
-        if not message.command:
-            return await message.reply_text(await self.bot.text(chat_id, "rules-blank-err"))
+        return {self.name: rules}
 
-        content = message.text.markdown.split(None, 1)
-        await self.rules_db.update_one(
-            {"chat_id": chat_id}, {"$set": {"rules": content[1]}}, upsert=True
+    async def on_plugin_restore(self, chat_id: int, data: MutableMapping[str, Any]) -> None:
+        await self.db.update_one({"chat_id": chat_id}, {"$set": data[self.name]}, upsert=True)
+
+    @command.filters(filters.admin_only)
+    async def cmd_setrules(self, ctx: command.Context) -> str:
+        chat = ctx.chat
+        if not ctx.input_raw:
+            return await self.text(chat.id, "rules-blank-err")
+
+        content = ctx.input_raw
+        ret, _ = await asyncio.gather(
+            self.text(chat.id, "rules-set", f"t.me/{self.bot.user.username}?start=rules_{chat.id}"),
+            self.db.update_one({"chat_id": chat.id}, {"$set": {"rules": content}}, upsert=True),
         )
-        return await message.reply_text(
-            await self.bot.text(
-                chat_id, "rules-set", f"t.me/{self.bot.username}?start=rules_{chat_id}"
-            )
+        return ret
+
+    @command.filters(filters.admin_only)
+    async def cmd_clearrules(self, ctx: command.Context) -> str:
+        chat = ctx.chat
+        ret, _ = await asyncio.gather(
+            self.text(chat.id, "rules-clear"), self.db.delete_one({"chat_id": chat.id})
         )
+        return ret
 
-    @listener.on("clearrules", admin_only=True)
-    async def clear_rules(self, message):
-        chat_id = message.chat.id
-        await self.rules_db.delete_one({"chat_id": chat_id})
-        await message.reply_text(await self.bot.text(chat_id, "rules-clear"))
-
-    @listener.on("rules")
-    async def rules(self, message):
-        chat_id = message.chat.id
-        content = await self.rules_db.find_one({"chat_id": chat_id})
+    async def cmd_rules(self, ctx: command.Context) -> Optional[str]:
+        chat = ctx.chat
+        content = await self.db.find_one({"chat_id": chat.id})
         if not content:
-            return await message.reply_text(await self.bot.text(chat_id, "rules-none"))
-        await message.reply_text(
-            await self.bot.text(chat_id, "rules-view-caption"),
+            return await self.text(chat.id, "rules-none")
+
+        await ctx.respond(
+            await self.text(chat.id, "rules-view-caption"),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
-                            text=await self.bot.text(chat_id, "rules-button"),
-                            url=f"t.me/{self.bot.username}?start=rules_{chat_id}",
+                            text=await self.text(chat.id, "rules-button"),
+                            url=f"t.me/{self.bot.user.username}?start=rules_{chat.id}",
                         )
                     ]
                 ]
             ),
         )
+        return None
