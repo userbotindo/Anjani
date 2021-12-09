a9f31661b0806586e18a6b51ece2d93eacb50ada
mrmissx <hi@mrmiss.me>
2021-12-09 11:15:37 +0700

    federation: Leave federation when bot demoted
    + fix joinfed when bot have no ban permission

--- anjani/plugins/federation.py
+++ anjani/plugins/federation.py
@@ -24,6 +24,7 @@ from pyrogram.errors import BadRequest, ChatAdminRequired, Forbidden
 from pyrogram.types import (
     CallbackQuery,
     Chat,
+    ChatMemberUpdated,
     InlineKeyboardButton,
     InlineKeyboardMarkup,
     Message,
@@ -64,6 +65,26 @@ class Federation(plugin.Plugin):
             banned = await self.is_fbanned(chat.id, new_member.id)
             if banned:
                 await self.fban_handler(message.chat, new_member, banned)
+
+    async def on_chat_member_update(self, update: ChatMemberUpdated) -> Optional[str]:
+        if update.old_chat_member.user.id != self.bot.uid:
+            return
+        if not (update.old_chat_member and update.new_chat_member):
+            return
+
+        if (
+            update.old_chat_member.can_restrict_members
+            and not update.new_chat_member.can_restrict_members
+        ):
+            chat = update.chat
+            fed_data = await self.get_fed_bychat(chat.id)
+            if not fed_data:
+                return
+            ret, _ = await asyncio.gather(
+                self.text(chat.id, "fed-autoleave", fed_data["name"], fed_data["_id"]),
+                self.db.update_one({"_id": fed_data["_id"]}, {"$pull": {"chats": chat.id}}),
+            )
+            await self.bot.client.send_message(chat.id, ret)
 
     @listener.filters(filters.regex(r"(rm|log)fed_(.*)"))
     async def on_callback_query(self, query: CallbackQuery) -> Any:
@@ -230,7 +251,7 @@ class Federation(plugin.Plugin):
         )
         return None
 
-    @command.filters(filters.admin_only)
+    @command.filters(filters.admin_only & filters.can_restrict)
     async def cmd_joinfed(self, ctx: command.Context, fid: Optional[str] = None) -> str:
         """Join a federation in chats"""
         chat = ctx.chat
