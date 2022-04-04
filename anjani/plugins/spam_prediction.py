52fb5669243b4400aaf9c49d5c78d8befd528f0d
mrmissx <hi@mrmiss.me>
2022-04-04 12:33:28 +0700

    spam_prediction: lower threshold to .8

--- anjani/plugins/spam_prediction.py
+++ anjani/plugins/spam_prediction.py
@@ -400,7 +400,7 @@ class SpamPrediction(plugin.Plugin):
         if util.tg.is_staff_or_admin(target):
             return
 
-        if probability >= 0.85:
+        if probability >= 0.8:
             if from_ocr:
                 alert = (
                     f"❗️**PHOTO SPAM ALERT**❗️\n\n"
