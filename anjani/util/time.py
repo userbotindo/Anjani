7fde61526a57079bf198efe1f7a74d19e5a7ff35
adekmaulana <adekzmaulana@gmail.com>
2022-09-03 16:51:38 +0700

    Util: silent this deepsource

--- anjani/util/time.py
+++ anjani/util/time.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import time as t
+import time as t  # skipcq: PYL-W0406
 from typing import Union
 
 
