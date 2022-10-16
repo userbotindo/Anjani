5c08aa434aa07f0831410299a3405aadfa5ab072
mrmissx <hi@mrmiss.my.id>
2022-10-16 11:00:20 +0700

    SpamShield: fix outgoing message check

--- anjani/plugins/spam_shield.py
+++ anjani/plugins/spam_shield.py
@@ -96,7 +96,7 @@ class SpamShield(plugin.Plugin):
     @listener.filters(filters.group)
     async def on_message(self, message: Message) -> None:
         """Checker service for message"""
-        if message.from_user.is_self:
+        if message.from_user and message.from_user.is_self:
             return
 
         chat = message.chat
