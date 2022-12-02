68541e7535951ad08f512f3bd8215ec26f3c3945
mrmissx <hi@mrmiss.my.id>
2022-12-02 22:41:29 +0700

    feat(topic): add topic plugin

---
+++ anjani/plugins/topic.py
@@ -0,0 +1,41 @@
+"""Bot stats plugin"""
+# Copyright (C) 2020 - 2022  UserbotIndo Team, <https://github.com/userbotindo.git>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import ClassVar, Optional
+
+from anjani import command, filters, plugin, util
+
+
+class Topics(plugin.Plugin):
+    name: ClassVar[str] = "Topics"
+
+    db: util.db.AsyncCollection
+
+    async def on_load(self) -> None:
+        self.db = self.bot.db.get_collection("CHATS")
+
+    @command.filters(filters.admin_only, aliases=["setdefaulttopic"])
+    async def cmd_setactiontopic(self, ctx: command.Context) -> Optional[str]:
+        """Set action topic"""
+        if not ctx.chat.is_forum:
+            return "This is not a forum chat."
+
+        await self.db.update_one(
+            {"chat_id": ctx.chat.id},
+            {"$set": {"action_topic": ctx.msg.message_thread_id}},
+            upsert=True,
+        )
+        return "This topic will be used for action topic."
