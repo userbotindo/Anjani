43e57c053d2e86b38f6314cbdfafe192a04b054c
tomyprs <tomiprs25@gmail.com>
2023-01-06 00:42:38 +0700

    Treewide: Change Licence Year
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

--- anjani/action.py
+++ anjani/action.py
@@ -1,5 +1,5 @@
 """Anjani base chat action"""
-# Copyright (C) 2020 - 2022  UserbotIndo Team, <https://github.com/userbotindo.git>
+# Copyright (C) 2020 - 2023  UserbotIndo Team, <https://github.com/userbotindo.git>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
