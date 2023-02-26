dc4f026c863c4ee838301955fd1e8c53a41dbae3
mrmissx <hi@mrmiss.my.id>
2023-02-26 19:43:07 +0700

    build: bump userbotindo_kit from 0.0.3 to 0.0.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ export $(grep -v '^#' config.env | xargs) > /dev/null 2>&1
 
 if [[ ! -z "$USERBOTINDO_ACCESS_TOKEN" ]]; then
     echo "Downloading userbotindo_kit..."
-    poetry run pip install git+https://${USERBOTINDO_ACCESS_TOKEN}@github.com/userbotindo/userbotindo_kit@v0.0.3
+    poetry run pip install git+https://${USERBOTINDO_ACCESS_TOKEN}@github.com/userbotindo/userbotindo_kit@v0.0.4
 fi
 
 poetry run anjani
