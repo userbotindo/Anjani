9a3b8ead1ac48b111974bf159743438d577bd797
mrmissx <hi@mrmiss.my.id>
2023-03-02 22:34:36 +0700

    fix: app not receiving stop signal

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ if [[ ! -z "$USERBOTINDO_ACCESS_TOKEN" ]]; then
     poetry run pip install git+https://${USERBOTINDO_ACCESS_TOKEN}@github.com/userbotindo/userbotindo_kit@v0.0.5
 fi
 
-poetry run anjani
+exec poetry run anjani
