c413f7dd592d7f2662464934be9cedff9f4293b8
Adek Maulana <adekzmaulana@gmail.com>
2023-03-26 21:29:41 +0700

    build(deps-internal): bump userbotindo_kit from 0.1.0-beta.6 to 0.1.0 (#271)
    Change-Id: I7ff6f337659caf31304721e8b131009d7271db54

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -z "$USERBOTINDO_ACCESS_TOKEN" ]]; then
     echo "Downloading userbotindo_kit..."
-    poetry run pip install git+https://${USERBOTINDO_ACCESS_TOKEN}@github.com/userbotindo/userbotindo_kit@v0.1.0-beta.6
+    poetry run pip install git+https://${USERBOTINDO_ACCESS_TOKEN}@github.com/userbotindo/userbotindo_kit@v0.1.0
 fi
 
 exec poetry run anjani
